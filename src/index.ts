f3f524f2d3086e18b548ebd214b27f896e04a34c
amoreX <rahmannihal72@gmail.com>
2025-07-28 19:17:41 +0530

    readme udpate

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import routes from "./routes";
 import { supabase } from "./utils/supabase.ts";
 const PORT = 8000;
 
@@ -13,6 +14,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
@@ -27,7 +30,7 @@ app.get("/health", async (req: Request, res: Response) => {
       return res.status(500).json({ message: "Database connection failed" });
     }
 
-    res.json({ message: "API and Supabase are healthy ✅" });
+    res.status(200).json({ message: "Healthy" });
   } catch (err) {
     console.error("❌ Health check failed:", err);
     res.status(500).json({ message: "Server or Supabase connection failed" });
