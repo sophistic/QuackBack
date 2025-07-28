51bd4458ac62fb898672cdc6072d7a616f001453
amoreX <rahmannihal72@gmail.com>
2025-07-28 18:49:28 +0530

    health endpoint

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require("express");
+const app = express();
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-const express = require("express");
+import express, { Request, Response } from "express";
 const app = express();
 const PORT = 8000;
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.json({ message: "API is healthy" });
 });
