da6ec3a2d214c6a346d5b1751251937a1d318f77
N!hal <71655709+amoreX@users.noreply.github.com>
2025-08-02 02:13:14 +0530

    Update notes.controller.ts

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -1,15 +1,20 @@
 import { Response, Request } from "express";
 import { updateNotes, getNotes } from "../services/notes.services";
+
 export const updateUserNotes = async (req: Request, res: Response) => {
   const { email, notes } = req.body;
   if (!email || !notes) {
-    return res
-      .status(500)
-      .json({ message: "Insufficient Data. Need email and Notes array." });
+    return res.status(400).json({
+      message: "Insufficient Data. Need email and notes array.",
+    });
   }
+
   try {
     const result = await updateNotes(email, notes);
-    return res.status(201).json(result);
+    if (!result) {
+      return res.status(500).json({ message: false });
+    }
+    return res.status(201).json({ message: true });
   } catch (err: any) {
     return res.status(500).json({ message: err.message });
   }
@@ -17,9 +22,19 @@ export const updateUserNotes = async (req: Request, res: Response) => {
 
 export const getUserNotes = async (req: Request, res: Response) => {
   const { email } = req.body;
+
+  if (!email) {
+    return res
+      .status(400)
+      .json({ message: "Email is required to retrieve notes." });
+  }
+
   try {
     const result = await getNotes(email);
-    return res.status(201).json(result);
+    if (!result) {
+      return res.status(404).json({ message: false });
+    }
+    return res.status(200).json(result);
   } catch (err: any) {
     return res.status(500).json({ message: err.message });
   }
